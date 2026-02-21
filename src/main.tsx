97226ea3a4c584c57b779ed1ec1352263338f6b2
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-21 10:09:24 +0600

    feat:(toast) configure the sooner for batter toast message

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes/AppRouter.tsx";
 import ReduxProvider from "./providers/ReduxProvider.tsx";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReduxProvider>
       <RouterProvider router={router} />
+      <Toaster />
     </ReduxProvider>
   </StrictMode>,
 );
