aa5d79452e377b4e106fb80626f888fd6393e87c
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-21 10:05:00 +0600

    feat(redux): configure store, add counter slice, actions, and selectors

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes/AppRouter.tsx";
+import ReduxProvider from "./providers/ReduxProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ReduxProvider>
+      <RouterProvider router={router} />
+    </ReduxProvider>
   </StrictMode>,
 );
