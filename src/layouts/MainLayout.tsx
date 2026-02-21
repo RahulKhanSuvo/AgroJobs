7191c136a00ecc7c039952af82606911f8c7c39d
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-21 10:47:09 +0600

    feat:added dark mode taggle

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,11 @@
+import { Navbar } from "@/components/layouts/Navbar";
 import { Outlet } from "react-router";
 
 export default function MainLayout() {
   return (
     <>
       <header>
-        <nav>this nav</nav>
+        <Navbar />
       </header>
       <main>
         <Outlet />
