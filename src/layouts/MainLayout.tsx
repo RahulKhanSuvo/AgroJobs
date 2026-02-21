80597b0d0803ce3ef24aa00575bc0e511f508e91
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-21 11:37:31 +0600

    feat:added reuseable components

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/layouts/Footer";
 import { Navbar } from "@/components/layouts/Navbar";
 import { Outlet } from "react-router";
 
@@ -10,7 +11,7 @@ export default function MainLayout() {
       <main>
         <Outlet />
       </main>
-      <footer>this is footer</footer>
+      <Footer />
     </>
   );
 }
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,4 +1,7 @@
 import MainLayout from "@/layouts/MainLayout";
+import Login from "@/pages/auth/Login";
+import SignUp from "@/pages/auth/SignUp";
+import Error from "@/pages/Error";
 import Home from "@/pages/Home/Home";
 import { createBrowserRouter } from "react-router";
 
@@ -11,7 +14,19 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
     ],
+  },
+  {
+    path: "*",
+    element: <Error />,
   },
 ]);
 export default router;
