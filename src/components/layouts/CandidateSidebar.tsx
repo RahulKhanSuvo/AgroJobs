55b9a33cc596c17f9e099714cc233db500631f02
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-02-28 20:20:42 +0600

    feat: added sidebar inatil setup

--- src/components/layouts/CandidateSidebar.tsx
+++ src/components/layouts/CandidateSidebar.tsx
@@ -12,6 +12,16 @@ import {
 } from "@/components/ui/sidebar";
 import { NavLink } from "react-router";
 import { Button } from "../ui/button";
+import {
+  Bookmark,
+  Building,
+  ChartBar,
+  Eye,
+  FileCheck,
+  History,
+  Settings,
+  User,
+} from "lucide-react";
 const data = {
   navMain: [
     {
@@ -19,25 +29,57 @@ const data = {
       url: "#",
       items: [
         {
-          title: "Installation",
-          url: "#",
+          title: "Dashboard",
+          url: "",
+          icon: <ChartBar />,
         },
         {
-          title: "Project Structure",
-          url: "#",
+          title: "Profile",
+          url: "profile",
+          icon: <User />,
+        },
+        {
+          title: "Applied Job",
+          url: "applied-job",
+          icon: <FileCheck />,
+        },
+        {
+          title: "Save Job",
+          url: "save-job",
+          icon: <Bookmark />,
+        },
+        {
+          title: "Profile Views",
+          url: "profile-views",
+          icon: <Eye />,
+        },
+        {
+          title: "Follow Company",
+          url: "followed-companies",
+          icon: <Building />,
+        },
+        {
+          title: "Job View History",
+          url: "recent-jobs",
+          icon: <History />,
+        },
+        {
+          title: "Setting",
+          url: "setting",
+          icon: <Settings />,
         },
       ],
     },
-    {
-      title: "Build Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
-        },
-      ],
-    },
+    // {
+    //   title: "Build Your Application",
+    //   url: "#",
+    //   items: [
+    //     {
+    //       title: "Routing",
+    //       url: "#",
+    //     },
+    //   ],
+    // },
   ],
 };
 
@@ -55,7 +97,10 @@ export function CandidateSidebar({
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
                     <SidebarMenuButton asChild>
-                      <NavLink to={item.url}>{item.title}</NavLink>
+                      <NavLink to={item.url}>
+                        {item.icon}
+                        <span>{item.title}</span>{" "}
+                      </NavLink>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
@@ -63,7 +108,11 @@ export function CandidateSidebar({
             </SidebarGroupContent>
           </SidebarGroup>
         ))}
-        <Button className="w-fit">Logout</Button>
+        <SidebarMenuItem className="p-2">
+          <SidebarMenuButton>
+            <Button className="w-fit ">Logout</Button>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
