e032aa0670fcba210a1b823f73421c372ecb6684
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-03-05 22:31:38 +0600

    refactor: redesign  candidate dashboard sidebar

---
+++ src/components/layouts/sidebarData.ts
@@ -0,0 +1,55 @@
+import { MdDashboard } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { SiGoogledocs } from "react-icons/si";
+import { TbBriefcase2Filled } from "react-icons/tb";
+import { FaFileCircleCheck } from "react-icons/fa6";
+import { RiGroup2Fill } from "react-icons/ri";
+import { IoNotifications, IoSettings } from "react-icons/io5";
+import { BiSolidMessageDetail } from "react-icons/bi";
+export const candidateSidebarData = [
+  {
+    title: "Dashboard",
+    href: "dashboard",
+    icon: MdDashboard,
+  },
+  {
+    title: "Profile",
+    href: "profile",
+    icon: FaUser,
+  },
+  {
+    title: "My CV",
+    href: "my-cv",
+    icon: SiGoogledocs,
+  },
+  {
+    title: "Saved Jobs",
+    href: "saved-jobs",
+    icon: TbBriefcase2Filled,
+  },
+  {
+    title: "My Applied Jobs",
+    href: "my-applied-jobs",
+    icon: FaFileCircleCheck,
+  },
+  {
+    title: "Followed Companies",
+    href: "followed-companies",
+    icon: RiGroup2Fill,
+  },
+  {
+    title: "Messages",
+    href: "messages",
+    icon: BiSolidMessageDetail,
+  },
+  {
+    title: "Notifications",
+    href: "notifications",
+    icon: IoNotifications,
+  },
+  {
+    title: "Settings",
+    href: "settings",
+    icon: IoSettings,
+  },
+];
--- src/layouts/CandidateLayout.tsx
+++ src/layouts/CandidateLayout.tsx
@@ -1,3 +1,4 @@
+import CandidateSideBar from "@/components/layouts/CandidateSideBar";
 import DashboardNavbar from "@/components/layouts/DashboardNavbar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet, ScrollRestoration } from "react-router";
@@ -5,12 +6,25 @@ import { Outlet, ScrollRestoration } from "react-router";
 export default function CandidateLayout() {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full flex-col">
-        <DashboardNavbar />
-        <main className="flex-1 p-4 bg-[#f5f7f9]">
-          <ScrollRestoration getKey={(location) => location.pathname} />
-          <Outlet />
-        </main>
+      <div className="flex h-screen w-full overflow-hidden">
+        {/* Sidebar */}
+        <div className="h-screen sticky top-0">
+          <CandidateSideBar />
+        </div>
+
+        {/* Right Section */}
+        <div className="flex flex-1 flex-col h-screen">
+          {/* Navbar */}
+          <div className="sticky top-0 z-50">
+            <DashboardNavbar />
+          </div>
+
+          {/* Scrollable content */}
+          <main className="flex-1 overflow-y-auto p-4 bg-[#f5f7f9]">
+            <ScrollRestoration getKey={(location) => location.pathname} />
+            <Outlet />
+          </main>
+        </div>
       </div>
     </SidebarProvider>
   );
